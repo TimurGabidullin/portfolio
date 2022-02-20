5fb1364ee2f465156bb60eec6c0c8915734a2a86
TimurGabidullin <pr.Gabidullin@gmail.com>
2022-02-20 21:51:59 +0400

    refactoring button

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,11 +1,13 @@
 import React from "react";
 import style from './Project.module.scss';
+import Button from "../../common/components/ button/Button";
 
 function Project(props) {
     return (
         <div className={style.project}>
             <div className={style.background} style={props.style}>
-                <a className={style.viewBtn} href="#">Смотреть</a>
+                <Button text={'VIEW'}/>
+                {/*<a className={style.viewBtn} href="#">Смотреть</a>*/}
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
