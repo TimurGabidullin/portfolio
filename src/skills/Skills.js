cff7c82670e0ac1f2e22f10ff1aa6e511dc0923a
TimurGabidullin <pr.Gabidullin@gmail.com>
2022-03-09 06:16:37 +0400

    refactoring sKills block

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -5,17 +5,20 @@ import Title from "../common/components/title/Title";
 import { faReact } from '@fortawesome/free-brands-svg-icons'
 import { faCss3Alt} from '@fortawesome/free-brands-svg-icons'
 import { faJs} from '@fortawesome/free-brands-svg-icons'
+import {faFileCode} from "@fortawesome/free-solid-svg-icons/faFileCode";
+
+
 
 function Skills() {
     return (
         <div className={style.skillsBlock}>
-            {/*<div className={`${styleContainer.container} ${style.skillsContainer}`}>*/}
             <div className={`${style.skillsContainer}`}>
                 <Title text={'Scills'}/>
                 <div className={style.skills}>
                     <Skill title={'Js'} description={'qqqqqq qqqqqqq qqqqqq qqqqqqqq qqqqqq qqqqq qqqqq'} icon={faJs} />
-                    <Skill title={'Css'} description={'qqqqqq qqqqqqq qqqqqq q'} icon={faCss3Alt}/>
+                    <Skill title={'html & Css'} description={'qqqqqq qqqqqqq qqqqqq q'} icon={faCss3Alt}/>
                     <Skill title={'React'} description={'qqqqqq qqqqqqq qqqqqq qqqqqqqq qqqqqq q'} icon={faReact}/>
+                    <Skill title={'html & Css'} description={'qqqqqq qqqqqqq qqqqqq qqqqqqqq qqqqqq q'} icon={faFileCode}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -3,15 +3,17 @@ import style from './Skill.module.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 
-
 function Skill(props) {
     return (
         <div className={style.skill}>
             <div className={style.icon}>
-                <FontAwesomeIcon icon={props.icon}
-                                 size='3x'
-                                 color={'#4e93e6'}
-                                 transform="down-2"/>
+                <div className={style.ic}>
+                    <FontAwesomeIcon icon={props.icon}
+                                     size='3x'
+                                     color={'#4e93e6'}
+                                     transform="down-1.5 left-1.5"
+                    />
+                </div>
             </div>
             <h3>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
